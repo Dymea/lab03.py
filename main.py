8441cc9c735ba6d5c04cddfaf896072beecc7aa2
Dymea <dxs5940@psu.edu>
2020-09-15 22:08:17 +0000

    changed if statement in def print_n

--- main.py
+++ main.py
@@ -7,13 +7,13 @@
 # Breakout: 11
 
 def sum_n(n):
-  if (n <= 1):
+  if (n >= 1):
     return 1
   else:
     return n+sum_n(n-1)
 
 def print_n(s, n):
-  if (n >= 1):
+  if (n != 0):
     print(s)
     print_n(s, n - 1)
   
