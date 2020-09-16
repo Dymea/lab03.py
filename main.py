dd3e2da50c535dd7993717be17800e718d991e34
Dymea <dxs5940@psu.edu>
2020-09-16 00:32:25 +0000

    added n>=0 and n<=0

--- main.py
+++ main.py
@@ -5,13 +5,13 @@
 # Breakout: 11
 
 def sum_n(n):
-  if (n <= 1 and n != 0):
+  if (n <= 1 and n >= 0):
     return 1
   else:
     return n+sum_n(n-1)
 
 def print_n(s, n):
-  if (n >= 1 and n != 0):
+  if (n >= 1 and n <= 0):
     print(s)
     print_n(s, n - 1)
   
